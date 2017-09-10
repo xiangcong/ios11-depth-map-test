7473d7647aab5fdb338e77291c1b64900f2922c5
jingjinzhou <jingjinzhou@tencent.com>
2017-09-10 17:13:54 +0800

    Initial Commit

---
+++ CameraTakePhotoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraTakePhotoDemo
+//
+//  Created by jingjinzhou on 10/9/17.
+//  Copyright © 2017年 jingjinzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraTakePhotoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraTakePhotoDemo
+//
+//  Created by jingjinzhou on 10/9/17.
+//  Copyright © 2017年 jingjinzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
